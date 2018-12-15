bda3890b22d6b769f8eb3f940bfe53f27f4ca80e
Mariano Kfuri <mkfuri@navent.com>
2018-12-14 22:41:48 -0300

    Agrego circuit breaker y fallback

--- api-gateway/src/main/java/api/gateway/client/ProductClient.java
+++ api-gateway/src/main/java/api/gateway/client/ProductClient.java
@@ -5,11 +5,13 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.CircuitBreaker;
 import io.reactivex.Single;
 
 import java.util.List;
 
 @Client(id = "product")
+@CircuitBreaker
 public interface ProductClient {
 
     @Get()
---
+++ api-gateway/src/main/java/api/gateway/fallback/ProductClientFallback.java
@@ -0,0 +1,44 @@
+package api.gateway.fallback;
+
+import api.gateway.client.ProductClient;
+import api.gateway.models.product.Product;
+import io.micronaut.retry.annotation.Fallback;
+import io.reactivex.Single;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Fallback
+@Slf4j
+public class ProductClientFallback implements ProductClient {
+
+    private static final Product defaultProduct = new Product();
+
+    static{
+        defaultProduct.setId("default");
+        defaultProduct.setName("Default");
+        defaultProduct.setPrice(0d);
+    }
+
+    @Override
+    public Single<List<Product>> listAll() {
+        log.info("Devolviendo la lista default");
+        return Single.just(Arrays.asList(defaultProduct));
+    }
+
+    @Override
+    public Single<Product> getById(String id) {
+        return null;
+    }
+
+    @Override
+    public Single<Product> create(Product product) {
+        return null;
+    }
+
+    @Override
+    public Single<Product> update(String id, Product product) {
+        return null;
+    }
+}
